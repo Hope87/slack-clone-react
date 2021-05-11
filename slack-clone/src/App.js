bfd9021ff66b234e3024ca8ef785660933db9d2a
Vitaliy <vitaliy0187@gmail.com>
2021-05-11 22:30:55 +0300

    add header component

---
+++ slack-clone/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ slack-clone/src/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAATSONvhLDuQKd1xv_ZtqCvsvITmM2SEk",
+//   authDomain: "slack-clone-cdfa5.firebaseapp.com",
+//   projectId: "slack-clone-cdfa5",
+//   storageBucket: "slack-clone-cdfa5.appspot.com",
+//   messagingSenderId: "659419097876",
+//   appId: "1:659419097876:web:dbc150cd9cd60e1b163001",
+// };
