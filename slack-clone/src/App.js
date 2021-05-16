c592ae4fa71cbe7699aaac4d2f252a0d6f7d50a3
Vitaliy <vitaliy0187@gmail.com>
2021-05-16 16:30:05 +0300

    add new components

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                <Chat />
+              </Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -20,3 +28,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
--- slack-clone/src/firebase.js
+++ slack-clone/src/firebase.js
@@ -1,8 +1,17 @@
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAATSONvhLDuQKd1xv_ZtqCvsvITmM2SEk",
-//   authDomain: "slack-clone-cdfa5.firebaseapp.com",
-//   projectId: "slack-clone-cdfa5",
-//   storageBucket: "slack-clone-cdfa5.appspot.com",
-//   messagingSenderId: "659419097876",
-//   appId: "1:659419097876:web:dbc150cd9cd60e1b163001",
-// };
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAATSONvhLDuQKd1xv_ZtqCvsvITmM2SEk",
+  authDomain: "slack-clone-cdfa5.firebaseapp.com",
+  projectId: "slack-clone-cdfa5",
+  storageBucket: "slack-clone-cdfa5.appspot.com",
+  messagingSenderId: "659419097876",
+  appId: "1:659419097876:web:dbc150cd9cd60e1b163001",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
